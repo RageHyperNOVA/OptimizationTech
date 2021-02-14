dea11767e4f1c223630337ccee77e26a4de3f9b2
Sudharsan Neelamegam <72805796+RageHyperNOVA@users.noreply.github.com>
2021-02-14 09:24:54 +0530

    Update EX_SEARCH_BOOM.cpp

--- EX_SEARCH_BOOM.cpp
+++ EX_SEARCH_BOOM.cpp
@@ -1,9 +1,6 @@
 /*
-
-
 EXHAUSTVE SEARCH METHOD
-
-
+Sudharsan Neelamegam
 */
 #include <bits/stdc++.h>
 #include <math.h>
@@ -12,7 +9,13 @@ EXHAUSTVE SEARCH METHOD
 using namespace std;
 
 
-
+float fun(float x)
+{
+    float result;
+    result = pow(x-1,2)-(0.01*pow(x,4)); //ENTER THE FUNCTION VALUE IN TERMS OF x AS PER IN QUESTION
+    return result;
+}
+float fun();
 int main()
 {
   float x1=0,x2,x3,b,a,n,dx,fx1,fx2,fx3,value;
@@ -24,22 +27,15 @@ int main()
   
   cout<<"iter   x1      x2      x3    fx1     fx2     fx3    cond"<<endl;
   dx=(b-a)/n;
-  for(int i=1;i<ivl-1;i++)
+  for(int i=0;i<ivl;i++)
   {
-      
-      
       x1=a;
       x2=a+dx;
       x3=a+(2*dx);
-      
-      
-        /*ENTER THE GIVEN FUNCTION HERE:*/
-        fx1=pow(x1-1,2)-(0.01*pow(x1,4)); //enter funtion f(x1) properly in terms of x1 
-        fx2=pow(x2-1,2)-(0.01*pow(x2,4)); //enter funtion f(x2) properly in terms of x1
-        fx3=pow(x3-1,2)-(0.01*pow(x3,4)); //enter funtion f(x3) properly in terms of x1
-        /*                              */
-       
-    if( (fx2<=fx1 && fx2<=fx3) )
+        fx1=fun(x1); 
+        fx2=fun(x2); 
+        fx3=fun(x3); 
+       if( (fx2<=fx1 && fx2<=fx3) )
     {   
         c=1;
         cout<<fixed;
@@ -48,8 +44,7 @@ int main()
     }
      cout<<fixed;
      cout<<i<<setprecision(2)<<"     "<<x1<<"    "<<x2<<"    "<<x3<<"   "<<fx1<<"    "<<fx2<<"    "<<fx3<<"    "<<c<<endl;
-  
-      a+=dx;
+     a+=dx;
   }
   cout<<"Req Range is : ["<<x1<<","<<x3<<"]";
 
