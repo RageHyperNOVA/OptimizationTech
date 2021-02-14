2097852b87026bb470ff333db7aa98e3b7ae6601
Sudharsan Neelamegam <72805796+RageHyperNOVA@users.noreply.github.com>
2021-02-14 09:05:50 +0530

    Update BOUNDING_PHASE_BOOM.cpp

--- BOUNDING_PHASE_BOOM.cpp
+++ BOUNDING_PHASE_BOOM.cpp
@@ -2,8 +2,10 @@
 
 
 BOUNDING PHASE METHOD
+Sudharsan Neelamegam
 
-SUDHARSAN NEELAMEGAM
+
+**** NOTE :Below float fun(float x), put the funation as given in the question in terms of x
 
 */
 #include <bits/stdc++.h>
@@ -13,18 +15,23 @@ SUDHARSAN NEELAMEGAM
 using namespace std;
 
 
-int fun(int x)
-{ int result;
-  result= pow(100-x,2);//ENTER FUNCTION IN TERMS OF X AS GIVEN IN QUESTION PROPERLY HERE
+float fun(float x)
+{ float result;
+  result=pow(100-x,2);//ENTER FUNCTION IN TERMS OF X AS GIVEN IN QUESTION PROPERLY HERE
+  return result;
+    
 }
-int fun();
+float fun();
 
 int main()
 {
-  int x0,k=0,xk,xkp,n,fxk,fxkp,fmm,fpp,d,f0,v=0;
-  int c,con=0;
-  cout<<"Enter the initial guess value x0 and increment value k:";
+  float x0,k=0,xk,xkp,n,fxk,fxkp,fmm,fpp,d,f0,v=0;
+  float c,con=0;
+  cout<<"Enter the initial guess value x0 and increment value :";
   cin>>x0>>d;
+  
+  
+  
   cout<<"k   xk      x(k+1)      fxk     fx(k+1)     cond"<<endl;
     fmm=x0-d;
     f0=x0;
@@ -32,17 +39,31 @@ int main()
     fpp=x0+d;
     xkp=fpp;
   while(v==0)
-  {   int a=fun(fmm),b=fun(xk),c=fun(xkp);   
-      if(b<=c)
+  {
+      
+      float a=fun(fmm),b=fun(xk),c=fun(xkp); 
+      if((a>=b) && (b>=c))
+      {
+          d=d*1;
+      }
+      else
+      {
+          d=d*(-1);
+      }
+      if(c>b)
       { con++;
         cout<<k<<"  "<<xk<<"  "<<xkp<<"  "<<b<<"  "<<c<<"  "<<con<<endl;
         break;
       }
-    cout<<k<<"  "<<xk<<"  "<<xkp<<"  "<<b<<"  "<<c<<"  "<<con<<endl;
+     cout<<k<<"  "<<xk<<"  "<<xkp<<"  "<<b<<"  "<<c<<"  "<<con<<endl;
+     
      xk=xkp;
      xkp=xk+((pow(2,k+1))*d);
      k++;
+     
   }
-  cout<<"Req range is: ("<<(xk-(pow(2,k-1)*d))<<","<<xkp<<")";
-  return 0; 
+  cout<<"Req range is: ("<<(xk+(pow(2,k-1)*d))<<","<<xkp<<")";
+
+return 0; 
 }
+
